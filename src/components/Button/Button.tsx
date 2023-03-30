4c51c3855c4673f8061354471607824b85f60c01
Nahrin <nahrin@famous.co>
2023-03-29 17:12:20 -0700

    added style

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -7,7 +7,7 @@ export interface ButtonProps {
 }
 
 const Button = ({ label }: ButtonProps) => {
-  return <button>{label}</button>
+  return <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">{label}</button>
 }
 
 export default Button;
