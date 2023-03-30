b57722264042994fe2b4e9e3fcc210671fffb062
Nahrin <nahrin@famous.co>
2023-03-29 17:22:13 -0700

    added rollup but with error

---
+++ .rollup.cache/Users/nahrin/ui-component-test-library/dist/components/Button/Button.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+var Button = function (_a) {
+    var label = _a.label;
+    return <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">{label}</button>;
+};
+export default Button;
+//# sourceMappingURL=Button.jsx.map
